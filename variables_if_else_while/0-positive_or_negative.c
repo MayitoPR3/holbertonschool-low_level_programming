ed14dab7920bcbbbe828569f14625ff4bb715712
Jose Mayo <mayitopr@hotmail.com>
2024-02-20 11:44:24 -0400

    0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -12,7 +12,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("%d",  n);
+	printf("%d", n);
 
 	if (n > 0)
 	{
@@ -26,7 +26,6 @@ int main(void)
 	{
 	printf( "is negative\n");
 	}
-
 	return (0);
 }
 
