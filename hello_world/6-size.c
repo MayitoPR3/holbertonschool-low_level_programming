62504efd90bebbbecde7078958d9b4e8902e23f8
Jose Mayo <mayitopr@hotmail.com>
2024-02-19 23:11:21 -0400

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
 * main
 * Return: Always 0.
 */
 
 int main(void)
+
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
