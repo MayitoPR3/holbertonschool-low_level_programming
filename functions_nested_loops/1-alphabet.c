fb2de2f82fccd6c2cf0e3392583aa84803a45921
Jose Mayo <mayitopr@hotmail.com>
2024-02-21 13:11:04 -0400

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Print the alphabet in lower case
+ * Return: Always 0
+ */
+int main(void)
+{
+	char alphabet = 'a';
+	while (alphabet <= 'z')
+	{
+		_putchar"alphabet";
+		alphabet++;
+	}
+	_putchar('\n');
+	print_alphabet();
+	return (0);
+}
