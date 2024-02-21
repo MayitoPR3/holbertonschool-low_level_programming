ee7eaff1ff355f6f811ddfe191b8c2694dcd4562
Jose Mayo <mayitopr@hotmail.com>
2024-02-21 12:02:39 -0400

    branch

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,2 +1,20 @@
-#include<stdio.h>
+#include"main.h"
 
+/**
+ * Main- Print _putchar
+ * Always Return 0 (Successful)
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
