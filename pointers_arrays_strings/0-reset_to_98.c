98da27e623a94c6d2cc80b79e41e5721b6100b26
Jose Mayo <mayitopr@hotmail.com>
2024-02-26 10:58:06 -0400

    0-reset_to_98.c

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *reset_to_98- function to tokae pointer to and in and update value to 98
+ *@n: is the 402
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
