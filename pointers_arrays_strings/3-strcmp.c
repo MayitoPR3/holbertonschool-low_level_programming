bf56d27292c77ab35c7ffad3fb8d28b134cf8a43
Jose Mayo <mayitopr@hotmail.com>
2024-02-28 13:26:03 -0400

    3-strcmp.c

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *@s1: string 1
+ *@s2: string 2
+ * Return: Always 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
