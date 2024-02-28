a9e0072cb789bf994f0d424cbe7abcf0c67a3ea9
Jose Mayo <mayitopr@hotmail.com>
2024-02-28 11:07:54 -0400

    2-strncpy.c

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *_strncpy- copy one string to another
+ *@dest: destination
+ *@src: source
+ *@n: number of bytes
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int j = 0, m = 0;
+
+	for (j = 0; src[j] && j < n; j++)
+	{
+		dest[j] = src[j];
+		m++;
+	}
+	for (j = m; j < n; j++)
+	{
+		dest[j] = '\0';
+	}
+	return (dest);
+}
