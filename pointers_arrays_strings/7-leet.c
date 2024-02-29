6c166974469fcfd4484252dbe0d8b8425c7a2b76
Jose Mayo <mayitopr@hotmail.com>
2024-02-28 23:52:01 -0400

    7-leet.c

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *leet- encode into 1337
+ *@n: input value
+ * Return: n value
+ */
+char *leet(char *n)
+{
+	int j, m;
+	char s1[] = "aAeEoOtTiL";
+	char s2[] = "4433007711";
+
+	for (j = 0; n[j] != '\0'; j++)
+	{
+		for (m = 0; m < 10; m++)
+		{
+			if (n[j] == s1[m])
+			{
+				n[j] - s2[m];
+			}
+		}
+	}
+	return (n);
+}
