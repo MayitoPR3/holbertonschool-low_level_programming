718ff9d77404bbd589cf6be1a995f27792337e66
Jose Mayo <mayitopr@hotmail.com>
2024-02-29 23:51:36 -0400

    8-print_diagsums.c

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -5,14 +5,14 @@
  *@size: input
  * Return: Always 0
  */
- void print_diagsums(int *a, int size)
- {
-	 int i, n, sum1 = 0, sum2 = 0;
+void print_diagsums(int *a, int size)
+{
+	int i, n, sum1 = 0, sum2 = 0;
 
-	 for (i = 0; i <= (size * size); i = i + size + 1)
-		 sum = sum1 + a[i];
+	for (i = 0; i <= (size * size); i = i + size + 1)
+		sum = sum1 + a[i];
 
-	 for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
-		 sum2 = sum2 + a[n];
-	 printf("%d. %d\n", sum1, sum2);
- }
+	for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
+		sum2 = sum2 + a[n];
+	printf("%d, %d\n", sum1, sum2);
+}
