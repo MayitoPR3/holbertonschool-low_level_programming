62081647a2c6fb45f92ceb915560264c61d82da1
Jose Mayo <mayitopr@hotmail.com>
2024-03-04 23:33:32 -0400

    3-factorial.c

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -2,17 +2,17 @@
 
 /**
  * factorial - returns given number
- * @n: number
- * Return:
+ * @n: number input
+ * Return: factorial number
  */
 int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
-	
+
 	else if (n == 0)
 		return (1);
-	
+
 	else
 		return (n * factorial(n - 1));
 }
