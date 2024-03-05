c38222505035095051d3eab8cf7083df5da7945f
Jose Mayo <mayitopr@hotmail.com>
2024-03-04 23:00:55 -0400

    3-factorial.c

---
+++ recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * int factorial - returns given number
+ * @n: number to return
+ * Return: given number
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	else
+		return (n * factorial(n -1));
+}
