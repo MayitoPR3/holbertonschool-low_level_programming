0034cc083aa161eb20b39bb0e1dc7176074a8311
Jose Mayo <mayitopr@hotmail.com>
2024-03-05 22:33:15 -0400

    0-whatsmyname.c

---
+++ argc_argv/0-whatmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - prints name
+ * @argc: number of cmd line argument
+ * @argv: array that contains the program
+ * Return: Always 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
