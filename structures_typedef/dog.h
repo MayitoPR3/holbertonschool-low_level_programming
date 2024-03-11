a1e9e202fd1021110eca0de38bed26bed903cd50
Jose Mayo <mayitopr@hotmail.com>
2024-03-11 10:32:46 -0400

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * struct dog - Structure for dog information
+ *
+ * Return: Always 0
+ */
+struct dog {
+  char *name;
+  float age;
+  char *owner;
+};
+
+typedef struct dog dog;
