006d02d07a34178e4062eb6ea05e42bcf65a1da5
Jose Mayo <mayitopr@hotmail.com>
2024-03-11 13:37:03 -0400

    1-init_dog.c

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -5,14 +5,13 @@
  * @name: Dog name
  * @age: Dog age
  * @owner: Dog owner
- * Return: Always 0
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
 }
